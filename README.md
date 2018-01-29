7fb247966b98f62d0a84053c2c6c03c8f0bda43b
ligq <liguoqiang@ebyf.com.cn>
2018-01-29 10:48:50 +0800

    2018-01-29 李国强 框架

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bootMVC
 springBoot MVC
+
+SpringBoot + MVC + Groovy模板，后期可能会改为SpringBoot + WebFlux + Groovy模板
---
+++ src/main/java/com/ligq/example/boot/mvc/service/Test.java
@@ -0,0 +1,11 @@
+package com.ligq.example.boot.mvc.service;
+
+/**
+ * class $classname
+ *
+ * @author ligq
+ * @description
+ * @date 2018/1/29, 10:47
+ */
+public class Test {
+}
